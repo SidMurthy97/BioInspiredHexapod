687066ac6f3f3ce638eb766f5803d4180fa20623
SidMurthy97 <murthy.siddhant@googlemail.com>
2021-01-03 19:33:19 +0000

    added mapping functions convert CPG output to motor position

--- RT_CPG_units.py
+++ RT_CPG_units.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math 
 import time 
+from oscillators import angle_to_position
 
-def hopf(t,z):
+
+def hopf(t):
     a,mu,omega = 1,1,math.pi
-    x,y = z
 
     x = math.sqrt(mu)*np.cos(omega*t)
     y = math.sqrt(mu)*np.sin(omega*t)
@@ -21,6 +22,17 @@ def vdp(t,z):
 
     return x,y
 
+def get_motor_commands(start):
+        
+    x,y = hopf(time.time() - start)
+    hip = 45*y
+    knee = 30*x if x > 0 else 0 
+
+    return angle_to_position(hip),angle_to_position(knee)
+
+
+
+
 if __name__ == "__main__":
     initial_conditions = [0,0]
     x,y = initial_conditions
@@ -29,16 +41,19 @@ if __name__ == "__main__":
     
     while time.time() - start < 10:
         
-        x,y = hopf(time.time() - start,[x,y])
-        xr.append(x)
-        yr.append(y)
+        x,y = hopf(time.time() - start)
+        hip = 45*y
+        knee = 30*x if x > 0 else 0 
+
+        xr.append(angle_to_position(hip))
+        yr.append(angle_to_position(knee))
 
 
-plt.figure()
-plt.plot(xr,yr)
+    plt.figure()
+    plt.plot(xr,yr)
 
-plt.figure()
-plt.plot(xr)
-plt.plot(yr)
-plt.show()
+    plt.figure()
+    plt.plot(xr)
+    plt.plot(yr)
+    plt.show()
     
