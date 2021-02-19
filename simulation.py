da2a7e697bc611803fac67c9e7d550fabc883b5d
SidMurthy97 <murthy.siddhant@googlemail.com>
2021-02-19 13:44:45 +0000

    Determined range of joints in the simulation. Next to integrate CPG unit into the simulation

--- simulation.py
+++ simulation.py
@@ -1,24 +1,49 @@
 import pybullet as p
 import time
 import pybullet_data
+from pprint import pprint  
+import math 
+
+
+def printJointInfo():
+    for i in range(nJoints):
+        print(p.getJointInfo(hexapod,i)[0:2])
+
+
+#set pybullet terms 
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) 
 p.setGravity(0,0,-10)
-planeId = p.loadURDF("plane.urdf")
-hexapod = p.loadURDF("C:\\Users\\user\\Documents\\year 5\\FYP\\robot_dart\\robots\\pexod.urdf")
+planeId = p.loadURDF("plane.urdf") #load plane
+startPos = [0,0,1]
+startOrientation = p.getQuaternionFromEuler([0,0,0])
+hexapod = p.loadURDF("C:\\Users\\user\\Documents\\year 5\\FYP\\robot_dart\\robots\\pexod.urdf",startPos, startOrientation,globalScaling=3) #load robot and make it bigger
+p.setRealTimeSimulation(1) #use system clock to step simulaton
 
 nJoints = p.getNumJoints(hexapod)
+states = [0,math.pi/4]
 
-states = [0,0.5]
+tol = 0.05 #set tolerance 
 
-p.setJointMotorControlArray(hexapod,range(nJoints),p.POSITION_CONTROL,[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
-#set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
+hips = [0,3,6,9,12,15]
+shoulder = [1,4,7,10,13,16]
+
 for i in range (1000):
-    p.stepSimulation()
-    p.setJointMotorControlArray(hexapod,range(nJoints),p.POSITION_CONTROL,[0,0,0,0,i%len(states),0,0,0,0,0,0,0,0,0,0,0,0,0])
+    index = i%len(states)
+    pos = states[index]
+    print("targert: ",pos)
+    p.setJointMotorControlArray(hexapod,shoulder,p.POSITION_CONTROL,[pos]*len(shoulder))
+    
+    while 1:
+        current_pos = p.getJointState(hexapod,1)[0]
+        print(current_pos)
+        if abs(current_pos - pos) < tol:
+            break
+    
+        
+    
+    print("-------------------------")
     time.sleep(1./240.)
+    
 
-
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-print(cubePos,cubeOrn)
 p.disconnect()
